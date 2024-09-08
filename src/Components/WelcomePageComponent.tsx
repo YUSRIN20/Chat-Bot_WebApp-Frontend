15b424a3961909e351a2bf19a5ad9c2617212040
YUSRIN20 <massyusrin123@gmail.com>
2024-09-09 01:15:33 +0530

    Changes in Welcome page

--- src/Components/WelcomePageComponent.tsx
+++ src/Components/WelcomePageComponent.tsx
@@ -72,12 +72,12 @@ const WelcomePageComponent: React.FC = () => {
                     </ul>
                 </div>
 
-                <div className="text-center">
+                {/* <div className="text-center">
                     <p className="text-sm text-gray-500">To get started, click on the "Speak" button and ask away!</p>
                     <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         Start Chatting
                     </button>
-                </div>
+                </div> */}
             </div>
         </div>
     );
